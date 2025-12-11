7e5fe1d9e567cc7d809750af8252067d0d1ad532
Damien Cauquil <virtualabs@gmail.com>
2025-12-12 00:10:27 +0100

    Updated pinout to fit the remapped version following our custom PCB v2.

--- components/retro-go/targets/tsingtao/config.h
+++ components/retro-go/targets/tsingtao/config.h
@@ -23,31 +23,31 @@
 #define RG_SCREEN_MARGIN_BOTTOM     0
 #define RG_SCREEN_MARGIN_LEFT       0
 #define RG_SCREEN_MARGIN_RIGHT      0
-#define RG_SCREEN_DATA0             GPIO_NUM_12
-#define RG_SCREEN_DATA1             GPIO_NUM_13
-#define RG_SCREEN_DATA2             GPIO_NUM_14
-#define RG_SCREEN_DATA3             GPIO_NUM_15
-#define RG_SCREEN_DATA4             GPIO_NUM_16
-#define RG_SCREEN_DATA5             GPIO_NUM_21
-#define RG_SCREEN_DATA6             GPIO_NUM_5
-#define RG_SCREEN_DATA7             GPIO_NUM_4
-#define RG_SCREEN_DATA8             GPIO_NUM_17
+#define RG_SCREEN_DATA0             GPIO_NUM_13
+#define RG_SCREEN_DATA1             GPIO_NUM_12
+#define RG_SCREEN_DATA2             GPIO_NUM_11
+#define RG_SCREEN_DATA3             GPIO_NUM_10
+#define RG_SCREEN_DATA4             GPIO_NUM_9
+#define RG_SCREEN_DATA5             GPIO_NUM_3
+#define RG_SCREEN_DATA6             GPIO_NUM_20
+#define RG_SCREEN_DATA7             GPIO_NUM_19
+#define RG_SCREEN_DATA8             GPIO_NUM_8
 #define RG_SCREEN_DATA9             GPIO_NUM_18
-#define RG_SCREEN_DATA10            GPIO_NUM_10
-#define RG_SCREEN_DATA11            GPIO_NUM_11
-#define RG_SCREEN_DATA12            GPIO_NUM_19
-#define RG_SCREEN_DATA13            GPIO_NUM_20
-#define RG_SCREEN_DATA14            GPIO_NUM_2
-#define RG_SCREEN_DATA15            GPIO_NUM_1
-#define RG_SCREEN_PCLK              GPIO_NUM_7
-#define RG_SCREEN_CS                GPIO_NUM_9
-#define RG_SCREEN_DC                GPIO_NUM_8
-#define RG_SCREEN_RST               GPIO_NUM_6
-#define RG_GPIO_LCD_BCKL            GPIO_NUM_40
+#define RG_SCREEN_DATA10            GPIO_NUM_17
+#define RG_SCREEN_DATA11            GPIO_NUM_16
+#define RG_SCREEN_DATA12            GPIO_NUM_15
+#define RG_SCREEN_DATA13            GPIO_NUM_7
+#define RG_SCREEN_DATA14            GPIO_NUM_6
+#define RG_SCREEN_DATA15            GPIO_NUM_5
+#define RG_SCREEN_PCLK              GPIO_NUM_14
+#define RG_SCREEN_CS                GPIO_NUM_47
+#define RG_SCREEN_DC                GPIO_NUM_21
+#define RG_SCREEN_RST               GPIO_NUM_48
+#define RG_GPIO_LCD_BCKL            GPIO_NUM_35
 
 /* Gamepad (MCP23017) */
 #define RG_I2C_DRIVER               2
-#define RG_GPIO_I2C_SCL             GPIO_NUM_38
+#define RG_GPIO_I2C_SCL             GPIO_NUM_40
 #define RG_GPIO_I2C_SDA             GPIO_NUM_39
 #define RG_GAMEPAD_I2C_MAP {\
     {RG_KEY_UP,     (1 << 0)},\
@@ -67,13 +67,13 @@
 // Status LED
 // #define RG_GPIO_LED                 GPIO_NUM_38
 
-#define RG_GPIO_SDSPI_MISO          GPIO_NUM_35
-#define RG_GPIO_SDSPI_MOSI          GPIO_NUM_36
-#define RG_GPIO_SDSPI_CLK           GPIO_NUM_37
+#define RG_GPIO_SDSPI_MISO          GPIO_NUM_1
+#define RG_GPIO_SDSPI_MOSI          GPIO_NUM_42
+#define RG_GPIO_SDSPI_CLK           GPIO_NUM_2
 #define RG_GPIO_SDSPI_CS            GPIO_NUM_41
 
 // External I2S DAC
-#define RG_GPIO_SND_I2S_BCK         GPIO_NUM_47
-#define RG_GPIO_SND_I2S_WS          GPIO_NUM_45 /* Actually, we should not use GPIO 45, PCB v2 uses 48. */
-#define RG_GPIO_SND_I2S_DATA        GPIO_NUM_42
+#define RG_GPIO_SND_I2S_BCK         GPIO_NUM_36
+#define RG_GPIO_SND_I2S_WS          GPIO_NUM_37 /* Actually, we should not use GPIO 45, PCB v2 uses 48. */
+#define RG_GPIO_SND_I2S_DATA        GPIO_NUM_38
 // #define RG_GPIO_SND_AMP_ENABLE      18
